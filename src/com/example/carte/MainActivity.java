648b07a333755552ea09a19bbe1c7688937bca84
toutoublue <yasmine12358@gmail.com>
2013-04-17 02:41:38 +0800

    payer button

--- src/com/example/carte/MainActivity.java
+++ src/com/example/carte/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.carte;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.*;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE2 = "com.example.carte.CHANGER";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +20,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void changer(View v){
+		Intent intent = new Intent(this, Preparation.class);
+    	String price = "jump to preparation";
+    	intent.putExtra(EXTRA_MESSAGE2, price);
+    	startActivity(intent);
 	}
 
 }
--- src/com/example/carte/Preparation.java
+++ src/com/example/carte/Preparation.java
@@ -1,11 +1,17 @@
 package com.example.carte;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class Preparation extends Activity {
-
+	public  final static String EXTRA_MESSAGE1 = "com.example.carte.PAYER";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +24,13 @@ public class Preparation extends Activity {
 		getMenuInflater().inflate(R.menu.preparation, menu);
 		return true;
 	}
+	
+    public void payer(View view){
+    	Intent intent = new Intent(this, RightWelcomeFragment.class);
+    	EditText  prix= (EditText) findViewById(R.id.prix);
+    	String price = prix.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE1, price);
+    	startActivity(intent);
+    }
 
 }
