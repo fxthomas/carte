1e0c925483eb5672b63ef884e0ccfad27874945e
François-Xavier Thomas <fx.thomas@gmail.com>
2013-04-16 14:06:52 +0200

    Fragments (Code)

--- src/com/example/carte/RightWelcomeFragment.java
+++ src/com/example/carte/RightWelcomeFragment.java
@@ -1,7 +1,14 @@
 package com.example.carte;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RightWelcomeFragment extends Fragment {
-
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.frag_right_welcome, container, false);
+	}
 }
