543c60c99dfc7a761f8981352c1249cc850d0598
Vlandeau <victor.landeau@telecom-paristech.fr>
2013-04-18 12:19:12 +0200

    Plats
    Ajout de plat dynamique

--- src/com/example/carte/MainActivity.java
+++ src/com/example/carte/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.carte;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE2 = "com.example.carte.CHANGER";
@@ -12,7 +16,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		Plat plat1 = new Plat("salade niçoise", "Excellente salade", 21, 3, 0x7f02000e);
+		Plat plat2 = new Plat("salade viennoise", "Excellente salade", 21, 3, 0x7f02000e);
+		Plat plat3 = new Plat("salade niçoise", "Excellente salade", 21, 3, 0x7f02000e);
+		ArrayList<Plat> plats = new ArrayList<Plat>();
+		plats.add(plat1);
+		plats.add(plat2);
+		plats.add(plat3);
+		
+		
     if (savedInstanceState == null) {
       LeftWelcomeFragment leftfrag = new LeftWelcomeFragment();
       RightWelcomeFragment rightfrag = new RightWelcomeFragment();
@@ -21,7 +34,14 @@ public class MainActivity extends Activity {
               .replace(R.id.fragment_right, rightfrag)
               .replace(R.id.fragment_left, leftfrag)
               .commit();
+      
+      ImageView image = new ImageView(this);
+      image.setImageResource(R.drawable.gateau);
+      LinearLayout l = (LinearLayout) findViewById(R.id.text_linearlayout);
+      l.addView(image);
+      
     }
+    
   }
 
 	@Override
--- src/com/example/carte/Plat.java
+++ src/com/example/carte/Plat.java
@@ -1,5 +1,67 @@
 package com.example.carte;
 
 public class Plat {
+	
+	String nom;
+	String description;
+	float prix;
+	int note;
+	int image;
+	
+	public Plat(String nom, String description, float prix, int note, int image) {
+		super();
+		this.nom = nom;
+		this.description = description;
+		this.prix = prix;
+		this.note = note;
+		this.image = image;
+	}
 
+	public Plat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public int getNote() {
+		return note;
+	}
+
+	public void setNote(int note) {
+		this.note = note;
+	}
+
+	public int getImage() {
+		return image;
+	}
+
+	public void setImage(int image) {
+		this.image = image;
+	}
+
+	
+	
 }
